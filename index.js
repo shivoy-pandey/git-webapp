1b6aea27780cbd40770deaff24055852598a7a90
shivoy-pandey <117445896+shivoy-pandey@users.noreply.github.com>
2023-04-19 13:15:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+const port = process.env.PORT || 8080;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
